7bcaaeff32fd0bd327677baf3f2ea5d0cbc5dfec
Davesssson <65303158+Davesssson@users.noreply.github.com>
2022-07-20 14:58:05 +0200

    details implementation

--- lib/constants/theme_data.dart
+++ lib/constants/theme_data.dart
@@ -4,15 +4,17 @@ import 'package:flutter/material.dart';
 class ThemeClass {
   static ThemeData light = ThemeData(
       brightness: Brightness.light,
-      primarySwatch: Colors.indigo,
+      primaryColor: Colors.grey.shade900,
+      primarySwatch: Colors.grey,
       accentColor: Colors.pink,
-      scaffoldBackgroundColor: Colors.grey.shade900
+      scaffoldBackgroundColor: Colors.grey.shade400
   );
 
   static ThemeData dark = ThemeData(
     brightness: Brightness.dark,
     primarySwatch: Colors.indigo,
+    primaryColor: Colors.brown.shade900,
     accentColor: Colors.pink,
-    scaffoldBackgroundColor: Colors.white
+    scaffoldBackgroundColor: Colors.brown
   );
 }
--- lib/ui/details_screen/variants/details_screen_mobile.dart
+++ lib/ui/details_screen/variants/details_screen_mobile.dart
@@ -22,20 +22,30 @@ class DetailsScreen_mobile extends HookConsumerWidget {
     final singleBarbershopState = ref.watch(barbershopSingleStateProvider(arg));
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: singleBarbershopState.when(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: singleBarbershopState.when(
               data: (data)=>
-              DetailWidget_mobile(ids: data.barbers),
+              SingleChildScrollView(
+                  child: DetailWidget_mobile(bs: data)
+              ),
               //  Column(
               //   children: [
               //     Text(data.name!),
               //     DetailWidget_mobile(ids:data.barbers!)
               //   ],
               // ),
-              error: (e,_)=>Text(e.toString()),
+              error: (e,_)=>Text("barbershopot nem tudtuk betölteni"),
               loading: ()=>CircularProgressIndicator(),
         ),
-      ),
+
     );
 
 
@@ -43,34 +53,175 @@ class DetailsScreen_mobile extends HookConsumerWidget {
 }
 
 class DetailWidget_mobile extends ConsumerWidget {
-  final ids;
-  const DetailWidget_mobile({Key? key,required this.ids}) : super(key: key);
+  final Barbershop bs;
+  const DetailWidget_mobile({Key? key,required this.bs}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final barbersState=ref.watch(barberListFromShopStateProvider(bs.barbers!));
+    final barbersContent=ref.watch(barberListFromShopContentProvider(bs.barbers!));
 
-    final barbersState=ref.watch(barberListFromShopStateProvider(ids));
-    final barbersContent=ref.watch(barberListFromShopContentProvider(ids));
-
-
+    //TODO EZT KELL ÁTÍRNI ÚGY, HOGY A WIDGET BUILDELJEN, CSAK AZ ADOTT KONTÉNER MUTASSON MÁST
     return barbersState.when(
         data: (data)=>data.isEmpty
         ? const Center(
-          child: Text("sajnos a barberek uresek"),
+          child: Text("sajnos a barbereket nem sikerült betölteni, vagy üresek"),
         )
-        :ListView.builder(
-          shrinkWrap: true,
-          itemCount: barbersContent.length,
-          itemBuilder:(BuildContext context, int index){
-            final barber = barbersContent[index];
-            return Text(barber.name!);
-          }
+        :Column(
+          children:[
+            Header(bs: bs),
+            Container(height: 50,color: Colors.red,child: Text("Ide jon még az elérhetőség"),),
+            BarberList(barbersContent: barbersContent),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8,8,0,8),
+              child: Container(
+                  alignment: Alignment.centerLeft,
+                  child:Text("Munkatársak",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white60,
+                        fontSize: 16
+                    ),
+                  )
+              ),
+            ),
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child:Row(
+                  children: [
+                    Container(height: 40,width: 100,color: Colors.blue,),
+                    Container(height: 40,width: 100,color: Colors.red,),
+                    Container(height: 40,width: 100,color: Colors.green,),
+                    Container(height: 40,width: 100,color: Colors.blue,),
+                    Container(height: 40,width: 100,color: Colors.grey,),
+                    Container(height: 40,width: 100,color: Colors.green,),
+                  ],
+                )
+            ),
+
+
+            Container(height: 300,color: Colors.red,),
+            Container(height: 300,color: Colors.blue,),
+            Container(height: 300,color: Colors.green,),
+
+
+
+         ]
         ),
         error: (e,_)=>Text(e.toString()),
         loading: ()=>CircularProgressIndicator(),
     );
 
 
+  }
+}
+
+class BarberList extends StatelessWidget {
+  const BarberList({
+    Key? key,
+    required this.barbersContent,
+  }) : super(key: key);
+
+  final List<Barber> barbersContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 151,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          itemCount: barbersContent.length,
+          itemBuilder:(BuildContext context, int index){
+            final barber = barbersContent[index];
+            //return Text(barber.name!);
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Material(
+                child: InkWell(
+                  onTap: () {
+                    // Navigator.of(context).push(
+                    //   MaterialPageRoute(
+                    //       builder: (_)=>StoryViewer(barber: barber)
+                    //   ),);
+                  },
+                  child: Container(
+                    color:Colors.black54,
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(barber.prof_pic!),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(barber.name!,style: TextStyle(color: Colors.white70),),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({
+    Key? key,
+    required this.bs,
+  }) : super(key: key);
+
+  final Barbershop bs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 3,
+          width: MediaQuery.of(context).size.width,
+          //color: Colors.red,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(bs.main_image!))),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height / 3,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Colors.black87,
+                    Colors.transparent
+                  ]
+              )
+          ),
+        ),
+        Positioned(
+            left: 10,
+            bottom: 30,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  bs.name!,
+                  style: const TextStyle(
+                      fontSize: 30,
+                      color: Colors.white70
+                  ),
+                ),
+              ],
+            )
+        ),
+      ],
+    );
   }
 }
 
