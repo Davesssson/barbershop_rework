db08c36759ced981244d6b3eedcf160404bf9704
Dávid Nagy <david.nagy@xenial.com>
2022-09-28 17:40:47 +0200

    login screen updated

--- lib/ui/login_screen/variants/login_screen_mobile.dart
+++ lib/ui/login_screen/variants/login_screen_mobile.dart
@@ -10,6 +10,152 @@ class LoginScreen_mobile extends HookConsumerWidget{
     late TextEditingController passwordController = TextEditingController();
 
     return Scaffold(
+      body:ListView(
+        children:[ Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.black
+            ),
+            Positioned(
+                bottom: 0,
+                //bottom: (MediaQuery.of(context).size.height*3)/4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(240, 240, 240, 1),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(150)
+                    )
+                  ),
+                  height: (MediaQuery.of(context).size.height*3)/4,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 40,
+                            fontWeight: FontWeight.w600
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(10))
+                          ),
+                          width: MediaQuery.of(context).size.width*0.8,
+                          height: MediaQuery.of(context).size.height/9,
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Email",style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w600),),
+                                ),
+                              ),
+                              TextFormField(
+
+                                style: TextStyle(color: Colors.black),
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.all(8),
+                                  suffixIcon: Icon(Icons.email),
+                                  suffixIconColor: Colors.black,
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.black)
+                                  ),
+                                  //labelText: "Label",
+                                  //labelStyle: TextStyle(
+                                  //  color:Colors.black
+                                  //),
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Enter a search term',
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Divider(height: 20,),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(Radius.circular(10))
+                          ),
+                          width: MediaQuery.of(context).size.width*0.8,
+                          height: MediaQuery.of(context).size.height/9,
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Password",style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w600),),
+                                ),
+                              ),
+                              TextFormField(
+
+                                style: TextStyle(color: Colors.black),
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.all(8),
+                                  suffixIcon: Icon(Icons.email),
+                                  suffixIconColor: Colors.black,
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.black)
+                                  ),
+                                  //labelText: "Label",
+                                  //labelStyle: TextStyle(
+                                  //  color:Colors.black
+                                  //),
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Enter a search term',
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Divider(height: 20,),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.all(Radius.circular(10))
+                          ),
+                          width: MediaQuery.of(context).size.width*0.8,
+                          height: MediaQuery.of(context).size.height/12,
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text("Login"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  //color: Colors.white,
+                )
+            ),
+
+          ],
+        ),
+                  ],
+      )
+    );
+
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping List'),
       ),
@@ -45,7 +191,6 @@ class LoginScreen_mobile extends HookConsumerWidget{
             authControllerState!=null
                 ? Text("be vagyok jelentkezve")
                 :Text("ki vagyok jelentkezve")
-
           ]
       ),
     );
