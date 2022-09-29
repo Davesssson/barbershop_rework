98cc2f51fdd1c817a57363fe445d1470be536ecd
Dávid Nagy <david.nagy@xenial.com>
2022-09-29 21:43:36 +0200

    listScreen saved

--- lib/ui/list_screen/variants/list_screen_mobile_final.dart
+++ lib/ui/list_screen/variants/list_screen_mobile_final.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_list/controllers/barbershop_controller/barbershop_featured_provider.dart';
 import 'package:flutter_shopping_list/models/barbershop/barbershop_model.dart';
+import 'package:flutter_shopping_list/ui/list_screen/variants/list_screen_mobile_final_proto.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../../controllers/barbershop_controller/barbershop_controller.dart';
 import '../../../controllers/barbershop_controller/barbershop_providers.dart';
 import '../../../controllers/city_controller/city_providers.dart';
 import 'dart:developer' as developer;
@@ -120,6 +122,12 @@ class ListScreen_mobile_final extends HookConsumerWidget {
               error: (error,_)=>Text("errpr in details screen"),
               loading: () => const Center(child: CircularProgressIndicator())
           ),
+          HorizontalList(
+              stateProvider:barbershopListStateProvider ,
+              contentProvider: barbershopListContentProvider,
+              shopToWatch: featuredShop
+          )
+
 
         ],
       ),
@@ -128,3 +136,49 @@ class ListScreen_mobile_final extends HookConsumerWidget {
   }
 }
 
+class HorizontalList extends ConsumerWidget{
+  const HorizontalList({
+    Key? key,
+    required this.stateProvider,
+    required this.contentProvider,
+    required this.shopToWatch,
+  }) : super(key: key);
+
+  final StateNotifierProvider<BarbershopListStateController, AsyncValue<List<Barbershop>>> stateProvider;
+  final Provider<List<Barbershop>> contentProvider;
+  final Provider<Barbershop> shopToWatch;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final stateProvider = ref.watch(this.stateProvider);
+    final contentProvider = ref.watch(this.contentProvider);
+
+    return stateProvider.when(
+        data: (data){
+          return data.isEmpty
+              ? SizedBox(height: 100, child: Text("Sanjnos nincsen adat a horizontalList-ben"),)
+              : SizedBox(
+                  height: MediaQuery.of(context).size.height/3,
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: contentProvider.length,
+                    separatorBuilder: (context, index){
+                      return const SizedBox(width: 20);
+                    },
+                    itemBuilder: (context, index) {
+                      final barbershop = contentProvider[index];
+                      return ProviderScope(
+                          overrides: [shopToWatch.overrideWithValue(barbershop)],
+                          child: ShopTile3(shopToWatch:shopToWatch)
+                      );
+                    },
+                  ),
+                );
+        },
+        error: (error,_)=>Text("errpr in details screen"),
+        loading: () => const Center(child: CircularProgressIndicator())
+    );
+  }
+
+}
