2dd54e34cf242553f28acfcec57f8cb2a5cb7a9d
Dávid Nagy <david.nagy@xenial.com>
2022-10-09 18:55:12 +0200

    updated choose time

--- lib/ui/book_screen/choose_time.dart
+++ lib/ui/book_screen/choose_time.dart
@@ -1,4 +1,6 @@
 
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_list/general_providers.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,26 +12,55 @@ import '../../controllers/timeslot/timeslot_controller.dart';
 import '../../controllers/timeslot/timeslot_providers.dart';
 import '../../models/availability/availability_model.dart';
 
-class chooseTime extends ConsumerWidget {
-  const chooseTime({Key? key}) : super(key: key);
+class chooseTime extends ConsumerStatefulWidget{
+  const chooseTime({Key? key}):super(key:key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<chooseTime> createState()=>_chooseTimeState();
+}
+
+
+class _chooseTimeState extends ConsumerState<chooseTime> {
+
+  final  List<DateTime> list =[];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    for(int i =0;i<30;i++){
+      list.add(DateTime.now().add(Duration(days:i)));
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final details = ref.watch(AvailabilityListStateProvider);
     final barbersState = ref.watch(barberListForShopStateProvider);
     final barbersContent = ref.watch(barberListForShopContentProvider);
     final chooseTime = ref.watch(AvailabilityListContentProvider);
     
-    List<DateTime> list = [];
-    
-    
     return Scaffold(
       body:details.when(
           data: (data) {
-            
+            print("eloszor");
+            print(list.toString());
+            data.forEach((element1) {
+              final asd = element1.id!.split("-");
+              final date = DateTime(int.parse(asd[0]),int.parse(asd[1]),int.parse(asd[2]));
+              list.removeWhere((element) {
+                List<String> asd2 = element.toString().split(" ");
+                List<String> asd22 = asd2[0].toString().split("-");
+                final date2 = DateTime(int.parse(asd22[0]),int.parse(asd22[1]),int.parse(asd22[2]));
+                return date==date2;
+              });
+            });
+            print("masodszor");
+            print(list.toString());
             return ListView(
               children:[
                   SfDateRangePicker(
+                    maxDate: DateTime.now().add(Duration(days:30)),
                     enablePastDates: false,
                     allowViewNavigation: true,
                     enableMultiView: false,
@@ -37,11 +68,12 @@ class chooseTime extends ConsumerWidget {
                       firstDayOfWeek: 7,
                       numberOfWeeksInView: 1,
                       blackoutDates:[
-                        ...data.map((e) { // ide még kell egy csekk, hogy csak akkor legyen engedélyezett hogyha van szabad timeslot
+                        ...list
+/*                        ...data.map((e) { // ide még kell egy csekk, hogy csak akkor legyen engedélyezett hogyha van szabad timeslot
                           final asd = e.id!.split("-");
                           print(asd);
                           return DateTime(int.parse(asd[0]),int.parse(asd[1]),int.parse(asd[2]));
-                        }).toList()
+                        }).toList()*/
                       ],
                     ),
                     onSelectionChanged: (DateRangePickerSelectionChangedArgs arg){
@@ -61,15 +93,30 @@ class chooseTime extends ConsumerWidget {
                     },
                 ),
                 Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 80,
-                  color: Colors.green,
-                  child: Text(chooseTime.slots!.length.toString()),
+                  height: MediaQuery.of(context).size.height/10,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      ...chooseTime.slots!.map((slot) {
+                        return InkWell(
+/*                          onTap: () {
+                            pushNewScreenWithRouteSettings(
+                              context,
+                              settings: RouteSettings(name: '/book'),
+                              screen: chooseTime(),
+                            );
+                          },*/
+                          child: Chip(
+                            label: Text(slot.start.toString()),
+                          ),
+                        );
+                      }).toList()
+                    ],
+                  ),
                 ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
 
-                )
+
+
               ]
             );
           },
@@ -79,12 +126,6 @@ class chooseTime extends ConsumerWidget {
     );
   }
 
-/*  void queryBlackoutDates(List<Availability> details ){
-    DateTime today = DateTime.now();
-    details.map((timeslot) {
-      if(DateTime(timeslot.id))
-    });*/
 
-
-  }
+}
 
