9c6c2d64fb451bb693489f6e109070a4aff1604c
Dávid Nagy <david.nagy@xenial.com>
2022-11-06 17:50:58 +0100

    Code refactoring CC

--- lib/controllers/barber_controller/barber_providers.dart
+++ lib/controllers/barber_controller/barber_providers.dart
@@ -1,23 +1,8 @@
-
 import 'package:flutter_shopping_list/repositories/barber_repository.dart';
 import 'package:riverpod/riverpod.dart';
 import 'dart:developer' as developer;
 import '../../models/barber/barber_model.dart';
 import 'barber_controller.dart';
-
-final barberListContentProvider = Provider<List<Barber>>((ref) {
-  developer.log("[barber_providers.dart][-][barberListContentProvider] - barberListContentProvider.");
-  final barbersListState = ref.watch(barberListStateProvider);
-  return barbersListState.maybeWhen(
-    data: (barbers) {
-      switch ("asd") {
-        default:
-          return barbers;
-      }
-    },
-    orElse: () => [],
-  );
-});
 
 
 final retrieveBarbersWorks = FutureProvider.family<List<Barber>,String>((ref,shopId) async {
@@ -32,11 +17,6 @@ final retrieveSingleBarber = FutureProvider.family<Barber,String>((ref,barberId)
   return barber;
 });
 
-final retrieveBookingsfrom = FutureProvider.family<List<Barber>,String>((ref,shopId) async {
-  final barbers = await ref.read(barberRepositoryProvider).retrieveBarbersFromShop2(shopId);
-  print("barbers.tostring"+barbers.toString());
-  return barbers;
-});
 
 //ezt át lehetne írni úgy, hogy a barberListProviderből returnol egy listát, az alapján, hogy a shopId megegyezik e kért shoppal
 final barberListForShopContentProvider = Provider<List<Barber>>((ref) {
--- lib/controllers/city_controller/city_providers.dart
+++ lib/controllers/city_controller/city_providers.dart
@@ -5,29 +5,6 @@ import 'city_controller.dart';
 
 final cityListFilterProvider = StateProvider<String>((_) => "");
 
-/*final cityListContentProvider = Provider<List<String>>((ref) {
-  developer.log("[city_providers.dart][-][cityListContentProvider] - cityListContentProvider.");
-  final itemListFilterState = ref.watch(cityListFilterProvider);
-  //final itemListState = ref.watch(cityListStateProvider);
-  return itemListState.maybeWhen(
-    data: (items) {
-      switch (itemListFilterState) {
-      //case CityListFilter.choosen:
-      //  return items.where((item) => item.obtained).toList();
-        default:
-          return items;
-      }
-    },
-    orElse: () => [],
-  );
-});*/
-
-/*final cityListStateProvider = StateNotifierProvider<CityListStateController, AsyncValue<List<String>>>((ref) {
-  developer.log("[city_providers.dart][-][cityListStateProvider] - cityListStateProvider");
-  return CityListStateController(ref.read);
-},
-);*/
-
 final cityListStateProvider2 = StateNotifierProvider<CityListStateController, AsyncValue<List<City>>>((ref) {
   developer.log("[city_providers.dart][-][cityListStateProvider] - cityListStateProvider");
   return CityListStateController.forModel(ref.read);
